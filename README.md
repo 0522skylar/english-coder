0844215cc41af35857534f2fc0b786e3c6eed0af
skylar <2031499619@qq.com>
2024-06-25 16:15:51 +0800

    chore: init project

---
+++ README.md
@@ -0,0 +1,12 @@
+
+# 运行
+
+node: v18.16.0
+```bash
+npm run dev
+# or
+yarn dev
+# or
+pnpm dev
+
+```
---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+// import { useEffect, useState } from 'react'
+// import { Inter } from 'next/font/google'
+// import styles from './home.module.scss'
+
+
+export default function Home() {
+
+  return (
+    <>
+      1234
+    </>
+  )
+}
